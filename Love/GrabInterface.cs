5f927e6400865ab21252d002d49650849d93c66d
burningsunrise <nvreaver@gmail.com>
2020-02-14 17:28:34 -0800

    Changed how we grab the interface and remember it

--- Love/GrabInterface.cs
+++ Love/GrabInterface.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using SharpPcap;
 using SharpPcap.Npcap;
 
@@ -8,13 +10,34 @@ namespace Love
     {
         public static ICaptureDevice MainDevice()
         {
+            var devFile = @$"{Environment.CurrentDirectory}/device.ini";
+            Console.WriteLine(devFile);
             var devices = CaptureDeviceList.Instance;
-            foreach (var dev in devices)
+            if (!File.Exists(devFile))
             {
-                if (dev.Name.ToLower()[0] == 'e')
-                    return dev;
+                Console.WriteLine("Please select your device, I'll try and remember it in the future.\n");
+                for (var i = 0; i < devices.Count; i++)
+                {
+                    Console.WriteLine($"{i}) {devices[i].Name}");
+                }
+
+                var input = Convert.ToInt32(Console.ReadLine());
+                using (StreamWriter outputFile =
+                    new StreamWriter(Path.Combine(Environment.CurrentDirectory, "device.ini")))
+                {
+                    outputFile.WriteLine(devices[input].Name);
+                }
+
+                return devices[input];
             }
-            
+            else
+            {
+                foreach (var dev in devices)
+                {
+                    if (dev.Name == File.ReadAllText(devFile))
+                        return dev;
+                }
+            }
             // Fails to find get first
             return devices[0];
         }
